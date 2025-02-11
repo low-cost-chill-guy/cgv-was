63f3ee6d13763ee11d134636c88a1ba4d1c38aed
okhi3945 <yhoka@naver.com>
2025-02-11 15:53:45 +0900

    first commit

---
+++ src/main/java/com/cloudwave/lowcostchillguy/LowcostchillguyApplication.java
@@ -0,0 +1,13 @@
+package com.cloudwave.lowcostchillguy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowcostchillguyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowcostchillguyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudwave/lowcostchillguy/LowcostchillguyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudwave.lowcostchillguy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowcostchillguyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
