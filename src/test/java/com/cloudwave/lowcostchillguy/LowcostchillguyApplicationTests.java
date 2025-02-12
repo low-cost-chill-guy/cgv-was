b4b6a01ffd5107cc16055c70aa6c069fb6f0ad19
okhi3945 <yhoka@naver.com>
2025-02-12 14:37:37 +0900

    2/12 build test fail -> add (classes = LowcostchillguyApplicationTests.class)

--- src/test/java/com/cloudwave/lowcostchillguy/LowcostchillguyApplicationTests.java
+++ src/test/java/com/cloudwave/lowcostchillguy/LowcostchillguyApplicationTests.java
@@ -2,8 +2,10 @@ package com.cloudwave.lowcostchillguy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = LowcostchillguyApplicationTests.class)
+@ActiveProfiles("test")
 class LowcostchillguyApplicationTests {
 
 	@Test
